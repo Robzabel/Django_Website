3de7f09ae3471b4402c9199115ef3e27b74b07b2
Robzabel <robzabel88@gmail.com>
2021-06-15 16:47:05 +0100

    database changes

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+#create the to do list model
 class ToDoList(models.Model):
     name = models.CharField(max_length=200)
 
-
+    #define a repr function to give information to the user
     def __repr__(self) -> str:
         return f"<Todo Name: {self.name}>"
-    
+
+#Create the Item model that will be linked to the ToDoList model 
+class Item(models.Model):
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)

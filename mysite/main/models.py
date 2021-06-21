6f6414e231378ecefe5290b1a541b343ef1ddbb2
Robzabel <robzabel88@gmail.com>
2021-06-21 13:47:11 +0100

    working with Database API

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -8,6 +8,21 @@ class ToDoList(models.Model):
     def __repr__(self) -> str:
         return f"<Todo Name: {self.name}>"
 
+    #define a str method so the API can retrieve the data
+    def __str__(self) -> str:
+        return f"<Todo Name: {self.name}>"
+
+
 #Create the Item model that will be linked to the ToDoList model 
 class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    complete = models.BooleanField(default=False)
+
+    #define a repr function to give information to the user
+    def __repr__(self) -> str:
+        return f"<Item: {self.text}>"
+
+    #define a str method so the API can retrieve the data
+    def __str__(self) -> str:
+        return f"<Item: {self.text}>"

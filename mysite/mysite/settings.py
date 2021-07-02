a20de41c5c006bf230c0b0da7020c2a40ba7cbac
Robzabel <robzabel88@gmail.com>
2021-07-02 14:39:05 +0100

    adding user registration and login authentication

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL="/"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from register import views as regView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', regView.register, name="register"),
     path('', include("main.urls")),
+    path('', include('django.contrib.auth.urls')),
 ]
